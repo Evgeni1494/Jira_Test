aeb91f42c93756457b7af55273b94fd0db01c9b6
Evgeni1494 <yoni1@hotmail.fr>
2023-03-17 15:07:15 +0100

    un super code secret

---
+++ code_test.py
@@ -0,0 +1,26 @@
+
+
+
+def style_de_coder():
+    genre_un_code = 0
+    for i in genre_un_code:
+        if i > 0:
+            i +=1
+        else:
+            i -=1
+        return i
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
